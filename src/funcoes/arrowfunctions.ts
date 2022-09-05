0c4e2b92419aeed05895e17439de005eb5e0cb52
Gilson Moreira dos Santos <gilson-ms@hotmail.com>
2022-09-05 14:25:17 -0300

    ArrowFunction

---
+++ src/funcoes/arrowfunctions.ts
@@ -0,0 +1,11 @@
+// Exemplo de arrow functions
+
+function retornaAPI(url: string): string {
+    return url;  
+}
+
+const retornoAPI = (url2: string): void =>{
+    console.log('URL DA API2: ', url2);
+}
+
+retornoAPI("https://www.uol.com.br");
